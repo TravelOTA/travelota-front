9d0119b4335320d1ceeca3014003acabe8be8921
Gabo <gopeguero7@gmail.com>
2026-02-20 00:09:28 -0500

    project created, eslint prettier plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default withNuxt({
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    ...eslintConfigPrettier.rules,
+  },
+});
