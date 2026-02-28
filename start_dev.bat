c97c4997d277580111df4efcb5ba768778669f1b
F3RR3R4 <99227306+F3RR3R4@users.noreply.github.com>
2026-02-27 22:38:44 -0500

    update start_dev.bat
    Actualizando para que instale dependencias

--- start_dev.bat
+++ start_dev.bat
@@ -4,6 +4,15 @@ echo ==========================================
 echo Iniciando Travelota Front en modo Desarrollo
 echo ==========================================
 
+:: Comprobar si pnpm está instalado
+where pnpm >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo [AVISO] 'pnpm' no se encuentra instalado en tu sistema.
+    echo Instalando pnpm globalmente mediante npm...
+    call npm install -g pnpm
+)
+
 echo.
 echo Verificando/Instalando dependencias (pnpm install)...
 call pnpm install
